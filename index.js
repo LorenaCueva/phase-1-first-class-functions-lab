023ded20c20f35ff9140324895d04f2fd06a232c
Lorena Cueva <lorenacueva@gmail.com>
2022-03-01 12:33:07 -0800

    commit tree 88575de0525dc16eb63b777c935a625e0d155112

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return [drivers[0],drivers[1]];
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+}
+
+function fareDoubler(fare) {
+   return createFareMultiplier(2)(fare);
+}
+
+function fareTripler(fare){
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers,action){
+    return action(drivers);
+}
